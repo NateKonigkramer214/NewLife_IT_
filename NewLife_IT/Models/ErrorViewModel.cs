f33714048b4164f57ffd2a6a7fe6143ffb43b7eb
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-03-06 13:08:29 +1300

    Add project files.

---
+++ NewLife_IT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewLife_IT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
